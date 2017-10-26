f606c71021cd4ba57ae2fe87b2dd5de755450b8c
Inesa-Rymko <32466767+Inesa-Rymko@users.noreply.github.com>
2017-10-27 00:01:39 +0300

    add abstact class Sweet

---
+++ NewYearGift/NewYearGift/Classes/Sweet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearGift.Classes
+{
+    public abstract class Sweet
+    {
+        public string name { get; private set; }
+        public double weight { get; private set; }
+        public double sugar { get; private set; }
+        public int pieceCount { get; set; }
+
+        public Sweet(string sweetName, double sweetWeight, double sweetSugar, int sweetCount)
+        {
+            this.name = sweetName;
+            this.weight = sweetWeight;
+            this.sugar = sweetSugar;
+            this.pieceCount = sweetCount;
+
+        }
+
+        public abstract void TypeSweet();
+    }
+}
+
---
+++ NewYearGift/NewYearGift/Types/TypeCoockie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearGift.Types
+{
+    public enum TypeCoockie
+    {
+        shortbread,
+        biscuit,
+        cracker
+    }
+}
